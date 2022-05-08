b86b974f224a6cfd27a1950a145cc9240ea820ab
Maverick Shreeve <maverickshreeve@gmail.com>
2022-05-07 20:53:29 -0700

    finished work project page

---
+++ src/components/Work.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import Bestman from '../assets/bestman.jpg';
+import Crypto from '../assets/crypto.jpg';
+import NFT from '../assets/nft.jpg';
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-gradient-to-r hover:bg-gradient-to-l from-purple-700 to-gray-500'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-5xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-7'>Here are some of my favorite projects</p>
+        </div>
+
+{/* Container */}
+          <div className='grid sm:grid-cols-2 md:grid-cols-1 gap-4'>
+
+            {/* Grid Item */}
+          <div
+          
+            style={{ backgroundImage:  `url(${Bestman})` }} 
+            className='shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS App using a API
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://bestman2-j3lonsw2d-maverick-shreeve.vercel.app//'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Crypto})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Tailwind Website
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://crypto-website-wheat.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Maverick-Shreeve/crypto-website/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        
+          <div
+            style={{ backgroundImage: `url(${NFT})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Python automation
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://opensea.io/collection/my-first-nfts-with-python/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Maverick-Shreeve/NFT-PROJECT/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
