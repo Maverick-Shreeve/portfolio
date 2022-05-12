43247389448d39cbaac5c221446aba4822305d37
Maverick Shreeve <maverickshreeve@gmail.com>
2022-05-12 12:13:38 -0700

    fixing resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
         <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a  
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://app.luminpdf.com/viewer/6276fb9f77e4ec345afd5103/'
+              href='https://app.luminpdf.com/viewer/627d5bb519d9e879988c5b04/'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
